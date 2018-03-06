0229e7687aed2c0ad9030565c86b2657b5f705fe
davidwu@ucsd.edu <davidwu@ucsd.edu>
2018-03-06 14:04:45 -0800

    m

---
+++ public/js/issuePage.js
@@ -0,0 +1,33 @@
+function set_json_callback(data){
+  // debugger;
+  //send back to home or change button text
+}
+
+function get_json_callback(data){
+  // debugger;
+  var parts = decodeURI(window.location.href).split('/');
+  var key = parts[parts.length - 1];
+  data[key]['task-status'] = true;
+  data[key].status = "COMPLETED";
+  $.post('/set_data_json', data, set_json_callback);
+}
+
+$(document).ready(function(e){
+  $('.not-filled-in').click(clickComplete);
+
+  function clickComplete(e){
+
+  	e.preventDefault();
+
+
+
+  	// "task-status": booleanStatus,
+  	// "status": taskStatusStr,
+  	// $.post('/issue', {data: JSON.stringify(this)}, function(data, status){
+  	// if(data.success){
+  	//    console.log("success");
+  	//    window.location.href="/homePage";
+  	// }
+    $.get('/get_data_json', get_json_callback)
+  }
+})
